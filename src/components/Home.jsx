cc453360b2e5876127ca6639aedc1001e2c5a119
AGBaby09 <agbotsem@gmail.com>
2024-07-02 14:20:46 -0700

    template

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import GridList from "./Grid/GridList";
 import RelBox from "./RelBox/RelBox";
@@ -15,8 +15,8 @@ import { OneInput } from "./Inputs/Inputs";
 const Home = () => {
 
   return (
-    <RelBox w={100} h={100} className={"center"}>
-      
+    <RelBox w={50} h={50} className={"center"}>
+      <Txt3 fs={20}>Hi There</Txt3>
     </RelBox>
   );
 };
